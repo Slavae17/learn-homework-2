2d30fab04c71febf161dffac05ce35a7a721bd73
Вячеслав Ершов <slavae17@mail.ru>
2022-05-25 15:30:54 +0300

    My hw about time and date

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -7,13 +7,22 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+from datetime import datetime, timedelta
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    dt_now = datetime.now() # Дата сегодня
+    print(f'Сегодняшняя дата - ', dt_now.strftime('%d.%m.%Y %H:%M')) 
+
+    delta1 = timedelta(days=1) 
+    delta30 = timedelta(days=30)
+
+    print(f'Дата вчерашняя - ', (dt_now - delta1).strftime('%d.%m.%Y %H:%M'))
+    print(f'Дата 30 дней назад - ', (dt_now - delta30).strftime('%d.%m.%Y %H:%M'))
+
 
 
 def str_2_datetime(date_string):
@@ -21,8 +30,11 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    date_string = '01/01/20 12:10:03.234567'
+    date_dt = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    return date_dt
 
 if __name__ == "__main__":
     print_days()
-    print(str_2_datetime("01/01/20 12:10:03.234567"))
+    print(str_2_datetime('01/01/20 12:10:03.234567'))
+
